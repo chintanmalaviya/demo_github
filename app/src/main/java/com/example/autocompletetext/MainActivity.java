5883cc96c4fbf46571869d0b4a3933537972075a
Chintan <c.k.patel4602@gmail.com>
2020-12-26 15:33:38 +0530

    initial commit

---
+++ app/src/main/java/com/example/autocompletetext/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.autocompletetext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView autoCompleteTextView;
+    String[] names;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        autoCompleteTextView = findViewById(R.id.auto);
+
+        names = new String[]{"Chintan","Amit","Hardik","Chirag","Vinay","Vikas"};
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,names);
+
+        autoCompleteTextView.setAdapter(arrayAdapter);
+    }
+}
